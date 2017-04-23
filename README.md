e7a08cd6d7106025e753fca4b70ce6a630e605cf
Michal Rus <m@michalrus.com>
2017-04-23 17:52:59 +0200

    Enable fanciness in bspwm

--- README.md
+++ README.md
@@ -2,12 +2,38 @@
 
 # hstatusbar
 
+## Why
+
+If you don’t like Bash, here’s a more principled way to define input for [lemonbar](https://github.com/LemonBoy/bar) or similar.
+
+## How to install
+
+The easiest way I know:
+
+1. Install [Nix](https://nixos.org/nix/).
+1. Clone the project.
+1. Run `nix-build`.
+1. The executable will be in `./result/bin/hstatusbar`.
+
+If you want to take some other route, keep in mind that dependencies in the `.cabal` file don’t have pinned versions, as these are based on versions available in a particular SHA1 of Nixpkgs.
+
 ## Examples
 
-```
-$ hstatusbar 'time is $(time.local "%a %d %b %H:%M:%S %Z") • $(time.universal "%H:%M %Z")'
-time is Sat 22 Apr 02:55:30 CEST • 00:55 UTC
-time is Sat 22 Apr 02:55:31 CEST • 00:55 UTC
-time is Sat 22 Apr 02:55:32 CEST • 00:55 UTC
-…
-```
+1. A basic one:
+
+   ```
+   $ hstatusbar 'time is $(time.local "%a %d %b %H:%M:%S %Z") • $(time.universal "%H:%M %Z")'
+   time is Sat 22 Apr 02:55:30 CEST • 00:55 UTC
+   time is Sat 22 Apr 02:55:31 CEST • 00:55 UTC
+   time is Sat 22 Apr 02:55:32 CEST • 00:55 UTC
+   …
+   ```
+
+2. *TODO: my screenshot*
+
+## Available interpolators
+
+* `time.local <fmt>`
+* `time.universal <fmt>`
+* `xtitle <max_length>` — uses [xtitle](https://github.com/baskerville/xtitle) to get current window title.
+* `bspwm <normal_pre> <normal_post> <selected_pre> <selected_post> <urgent_pre> <urgent_post> [<icon0> …]` — uses [Bspwm](https://github.com/baskerville/bspwm)’s `bspc subscribe` to get the status of workspaces. (*Note: I’ve only ever used a single screen, so a simple PR might be needed to get this working sensibly on multiple screens.*)
--- src/HStatusBar/Bspwm.hs
+++ src/HStatusBar/Bspwm.hs
@@ -3,13 +3,81 @@ module HStatusBar.Bspwm
   ) where
 
 import           Control.Concurrent
+import           Control.Monad      (join)
 import           HStatusBar.Decl
 import           HStatusBar.Types
 import           System.Process     (proc)
+import           Text.Megaparsec
 
 bspwm :: Decl
-bspwm = bspwm_ <$ decl "bspwm"
+bspwm =
+  bspwm_ <$> (decl "bspwm" *> arg) <*> arg <*> arg <*> arg <*> arg <*> arg <*>
+  many arg
 
--- FIXME: parse its output and paint something fancy
-bspwm_ :: Module
-bspwm_ = processByLine (proc "bspc" ["subscribe"]) $ flip writeChan
+-- FIXME: this is terrible, String madness.
+bspwm_ :: String
+       -> String
+       -> String
+       -> String
+       -> String
+       -> String
+       -> [String]
+       -> Module
+bspwm_ normalPre normalPost selectedPre selectedPost urgentPre urgentPost icons =
+  processByLine (proc "bspc" ["subscribe"]) $ \line ch ->
+    case parseMaybe parseLine line of
+      Just (BspLine _ wspaces _) ->
+        writeChan ch $
+        join $ showWspace <$> (wspaces `zip` (icons ++ repeat ""))
+      _ -> pure ()
+  where
+    showWspace :: (Workspace, String) -> String
+    showWspace (wspace, icon) =
+      case tpe . state $ wspace of
+        WUrgent -> urgentPre ++ nameIcon ++ urgentPost
+        _
+          | isSelected . state $ wspace ->
+            selectedPre ++ nameIcon ++ selectedPost
+        WOccupied -> normalPre ++ nameIcon ++ normalPost
+        _ -> ""
+      where
+        nameIcon =
+          if icon == ""
+            then name wspace
+            else icon
+
+data WType
+  = WOccupied
+  | WFree
+  | WUrgent
+
+data WState = WState
+  { tpe :: WType
+  , isSelected :: Bool
+  }
+
+data Workspace = Workspace
+  { state :: WState
+  , name :: String
+  }
+
+data BspLine =
+  BspLine String
+          [Workspace]
+          [String]
+
+parseLine :: Parsec Dec String BspLine
+parseLine = BspLine <$> many (noneOf ":") <*> many (try wspace) <*> many rest
+  where
+    wspace :: Parsec Dec String Workspace
+    wspace = Workspace <$> (char ':' *> wstate) <*> some (noneOf ":")
+    wstate :: Parsec Dec String WState
+    wstate =
+      (char 'O' *> pure (WState WOccupied True)) <|>
+      (char 'o' *> pure (WState WOccupied False)) <|>
+      (char 'F' *> pure (WState WFree True)) <|>
+      (char 'f' *> pure (WState WFree False)) <|>
+      (char 'U' *> pure (WState WUrgent True)) <|>
+      (char 'u' *> pure (WState WUrgent False))
+    rest :: Parsec Dec String String
+    rest = char ':' *> many (noneOf ":")
